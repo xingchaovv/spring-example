25a4037f58f27ebbc4db42e247fcdcef7bce5332
Xingchao Zhang <xingchaovv@gmail.com>
2024-02-05 22:21:12 +0800

    更新 logback 配置。

--- example55/src/main/java/example/UserController.java
+++ example55/src/main/java/example/UserController.java
@@ -1,13 +1,16 @@
 package example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class UserController {
 
     @GetMapping("/user/count")
     public Integer count() {
+        log.info("开始执行：控制器UserController方法count");
         return 100;
     }
 }
